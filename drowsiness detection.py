7d03c78433fed214e296cff2da686f96b11cf6e5
anuragjoshi3519 <anurag_joshi@outlook.com>
2020-08-21 20:30:31 +0530

    model updated

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -1,53 +1,51 @@
-import cv2
+import cv2 as opencv
 import os
 from tensorflow.keras.models import load_model
 import numpy as np
 from pygame import mixer
 import time
 
-
 mixer.init()
-sound = mixer.Sound('alarm.wav')
+buzzer = mixer.Sound('buzzer.wav')
 
-face = cv2.CascadeClassifier('haarcascade_files/haarcascade_frontalface_alt.xml')
-leye = cv2.CascadeClassifier('haarcascade_files/haarcascade_lefteye_2splits.xml')
-reye = cv2.CascadeClassifier('haarcascade_files/haarcascade_righteye_2splits.xml')
-
-
+face = opencv.CascadeClassifier('haarcascade_files/haarcascade_frontalface_alt.xml')
+leye = opencv.CascadeClassifier('haarcascade_files/haarcascade_lefteye_2splits.xml')
+reye = opencv.CascadeClassifier('haarcascade_files/haarcascade_righteye_2splits.xml')
 
 lbl=['Close','Open']
 
-model = load_model('minecnnCat2.h5')
+model = load_model('cnnCat2.h5')
+
 path = os.getcwd()
-cap = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+cap = opencv.VideoCapture(0)  #0-webcam, displays what is showing on the webcam
+font = opencv.FONT_HERSHEY_COMPLEX_SMALL  #sets the font
 count=0
 score=0
-thicc=2
+thickness=2 
 rpred=[99]
 lpred=[99]
 
 while(True):
-    ret, frame = cap.read()
-  	
+    ret, frame = cap.read()  #ret - true/false whether it was able to read the file or not, frame - one frame object
+
     height,width = frame.shape[:2] 
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+    gray = opencv.cvtColor(frame, opencv.COLOR_BGR2GRAY)  #converting video to grayscale
+
     faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
     left_eye = leye.detectMultiScale(gray)
     right_eye =  reye.detectMultiScale(gray)
 
-    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+    opencv.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=opencv.FILLED ) #draws a rectangle on the image for score and label
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
+        opencv.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
 
     for (x,y,w,h) in right_eye:
         r_eye=frame[y:y+h,x:x+w]
         count=count+1
-        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
-        r_eye = cv2.resize(r_eye,(24,24))
+        r_eye = opencv.cvtColor(r_eye,opencv.COLOR_BGR2GRAY)
+        r_eye = opencv.resize(r_eye,(24,24))
         r_eye= r_eye/255
         r_eye=  r_eye.reshape(24,24,-1)
         r_eye = np.expand_dims(r_eye,axis=0)
@@ -61,8 +59,8 @@ while(True):
     for (x,y,w,h) in left_eye:
         l_eye=frame[y:y+h,x:x+w]
         count=count+1
-        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
-        l_eye = cv2.resize(l_eye,(24,24))
+        l_eye = opencv.cvtColor(l_eye,opencv.COLOR_BGR2GRAY)  
+        l_eye = opencv.resize(l_eye,(24,24))
         l_eye= l_eye/255
         l_eye=l_eye.reshape(24,24,-1)
         l_eye = np.expand_dims(l_eye,axis=0)
@@ -75,33 +73,37 @@ while(True):
 
     if(rpred[0]==0 and lpred[0]==0):
         score=score+1
-        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        opencv.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,opencv.LINE_AA)
     # if(rpred[0]==1 or lpred[0]==1):
     else:
         score=score-1
-        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        opencv.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,opencv.LINE_AA)
     
         
     if(score<0):
         score=0   
-    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    opencv.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,opencv.LINE_AA)
+    if(score<=7):
+        buzzer.stop()
     if(score>15):
         #person is feeling sleepy so we beep the alarm
-        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+        opencv.imwrite(os.path.join(path,'image.jpg'),frame)  #saves one frame on the device
         try:
-            sound.play()
+            buzzer.play()
             
         except:  # isplcap.read()aying = False
             pass
-        if(thicc<16):
-            thicc= thicc+2
+        if(thickness<16):
+            thickness= thickness+2
         else:
-            thicc=thicc-2
-            if(thicc<2):
-                thicc=2
-        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+            thickness=thickness-2
+            if(thickness<2):
+                thickness=2
+        opencv.rectangle(frame,(0,0),(width,height),(0,0,255),thickness) #draws a rectangle
+    
+    opencv.imshow('frame',frame)  #displays on the screen what is being captured by the webcam
+    if opencv.waitKey(1) & 0xFF == ord('q'):  #when q is pressed then capturing will terminate
         break
+        
 cap.release()
-cv2.destroyAllWindows()
+opencv.destroyAllWindows()
--- model.py
+++ model.py
@@ -2,57 +2,67 @@ import os
 from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt 
 import numpy as np
-from tensorflow.keras.utils.np_utils import to_categorical
+from tensorflow.keras.utils import to_categorical
 import random,shutil
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
 
+from google.colab import drive
+drive.mount('/content/drive')
 
-def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
+train_image_gen = ImageDataGenerator(
+    rescale = 1/255.0,
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    shear_range = 0.2,
+    zoom_range = 0.3,
+    horizontal_flip = True,
+    fill_mode = 'nearest'
+)
 
-    return gen.flow_from_directory(dir,batch_size=batch_size,shuffle=shuffle,color_mode='grayscale',class_mode=class_mode,target_size=target_size)
+train_gen = train_image_gen.flow_from_directory(
+    "/content/drive/My Drive/data/train",
+    target_size = ((24,24)),
+    batch_size = 64,
+    class_mode = 'categorical',
+    color_mode='grayscale'
+)
 
-BS= 32
-TS=(24,24)
-train_batch= generator('data/train',shuffle=True, batch_size=BS,target_size=TS)
-valid_batch= generator('data/valid',shuffle=True, batch_size=BS,target_size=TS)
-SPE= len(train_batch.classes)//BS
-VS = len(valid_batch.classes)//BS
-print(SPE,VS)
+test_image_gen = ImageDataGenerator(
+    rescale = 1/255.0,
+)
 
+test_gen = test_image_gen.flow_from_directory(
+    "/content/drive/My Drive/data/test",
+    target_size = ((24,24)),
+    batch_size = 16,
+    class_mode = 'categorical',
+    color_mode='grayscale'
+)
 
-# img,labels= next(train_batch)
-# print(img.shape)
+model = Sequential()
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(24,24,1)))  #no. of filters, kernel size
+model.add(BatchNormalization()) #mean 0 - standard deviation 1
+model.add(MaxPooling2D(2,2))  #Downsamples by taking the maximum value over the window defined by pool_size, reduces the size, increases speed
+model.add(Dropout(0.4)) #reduces overfitting and increases generalization
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.4))  #Fraction of the input units to drop
+model.add(Flatten())
+model.add(Dense(32,activation='relu'))  #no. of units
+model.add(Dropout(0.4))
+model.add(Dense(2,activation='softmax'))
+model.summary()
+# 52,562 parameters out of which 192 non trainable parameters
+model.compile(loss="categorical_crossentropy",optimizer="adam",metrics=["accuracy"])
 
-model = Sequential([
-    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),
-    MaxPooling2D(pool_size=(1,1)),
-    Conv2D(32,(3,3),activation='relu'),
-    MaxPooling2D(pool_size=(1,1)),
-#32 convolution filters used each of size 3x3
-#again
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(pool_size=(1,1)),
+#Adam is an optimization algorithm that can be used instead of the classical stochastic gradient descent procedure to update network weights iterative based in training data.
 
-#64 convolution filters used each of size 3x3
-#choose the best features via pooling
-    
-#randomly turn neurons on and off to improve convergence
-    Dropout(0.25),
-#flatten since too many dimensions, we only want a classification output
-    Flatten(),
-#fully connected to get all relevant data
-    Dense(128, activation='relu'),
-#one more dropout for convergence' sake :) 
-    Dropout(0.5),
-#output a softmax to squash the matrix into output probabilities
-    Dense(2, activation='softmax')
-])
+model.fit_generator(train_gen, validation_data=test_gen,epochs=5,steps_per_epoch=4321//64,validation_steps=525//16)
 
-model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-
-model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
-
-model.save('cnnCat2.h5', overwrite=True)
+model.save('../content/models/cnnCat2.h5', overwrite=True)
 
